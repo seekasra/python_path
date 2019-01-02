35701d4fb106cf288c94b102ba8c7df1de288414
Kasra Kassai <seekasra@gmail.com>
2019-01-02 23:24:15 +0000

    add stack.py

---
+++ algorithms/stack.py
@@ -0,0 +1,22 @@
+# try out the Python stack functions
+
+# TODO: create a new empty stack
+
+stack = []
+
+# TODO: push items onto the stack
+
+
+stack.append(10)
+stack.append(11)
+stack.append(12)
+stack.append(13)
+# TODO: print the stack contents
+
+print(stack)
+
+# TODO: pop an item off the stack
+
+
+print(stack.pop(2))
+print(stack)
