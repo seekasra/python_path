56cf7ff50290758c4f7deb7c119d32f99e9ca4dd
Kasra Kassai <seekasra@gmail.com>
2019-01-02 23:29:11 +0000

    add queue.py

---
+++ algorithms/queue.py
@@ -0,0 +1,24 @@
+# try out the Python queue functions
+from collections import deque
+
+# TODO: create a new empty deque object that will function as a queue
+queue = deque()
+
+
+# TODO: add some items to the queue
+
+
+queue.append(10)
+queue.append(11)
+queue.append(12)
+queue.append(13)
+
+# TODO: print the queue contents
+
+print(queue)
+
+# TODO: pop an item off the front of the queue
+
+queue.popleft()
+
+print(queue)
