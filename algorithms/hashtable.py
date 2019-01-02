62daceb7262f069e2c54259635ad014fa44c8739
Kasra Kassai <seekasra@gmail.com>
2019-01-02 23:45:05 +0000

    add hashtable.py

---
+++ algorithms/hashtable.py
@@ -0,0 +1,33 @@
+# demonstrate hashtable usage
+
+
+# TODO: create a hashtable all at once
+
+items1 = dict({"key1":1 ,"key2": 2,"key3": "three"})
+
+print(items1)
+
+# TODO: create a hashtable progressively
+
+items2 = {}
+
+items2["key1"] = 1
+items2["key2"] = 2
+items2["key3"] = 3
+items2["key4"] = 4
+
+print(items2)
+
+# TODO: try to access a nonexistent key
+
+#print(items2["key10"])
+
+# TODO: replace an item
+
+items2["key4"] = 10
+print(items2)
+
+# TODO: iterate the keys and values in the dictionary
+
+for key, value in items2.items():
+    print("Key: ", key, " Value: ", value)
