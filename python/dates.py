9157438ef28127bc2a9c70e5739ac9ab840647e7
Kasra Kassai <seekasra@gmail.com>
2019-01-03 23:58:49 +0000

    add dates.py

---
+++ python/dates.py
@@ -0,0 +1,34 @@
+#
+# Example file for working with date information
+#
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+def main():
+  ## DATE OBJECTS
+  # Get today's date from the simple today() method from the date class
+  today = date.today()
+  print("Today's date is ", today)
+
+  # print out the date's individual components
+  print("date commponents", today.day, today.month, today.year)
+  
+  # retrieve today's weekday (0=Monday, 6=Sunday)
+  #print("Today's weekday number is: ", today.weekday())
+  #day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  #print("Today is :", day[today.weekday()], "!" )
+  
+  ## DATETIME OBJECTS
+  # Get today's date from the datetime class
+  today = datetime.now()
+  print(today)
+
+  # Get the current time
+  t = datetime.time(datetime.now())
+  print(t)
+
+  
+if __name__ == "__main__":
+  main()
+  
