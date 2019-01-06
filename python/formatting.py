41dcc393500317f21f707a49fe7cb8e11d0d2f13
Kasra Kassai <seekasra@gmail.com>
2019-01-06 22:36:49 +0000

    add time and locale using datetime

--- python/formatting.py
+++ python/formatting.py
@@ -7,18 +7,14 @@ from datetime import datetime
 def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
-
+  now = datetime.now()
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+  print(now.strftime("The current year is %y"))
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
-
-
-  #### Time Formatting ####
-  
-  # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Locale is: %c, date: %x and time %X"))
 
 
 
