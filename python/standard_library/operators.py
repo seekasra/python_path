96bc1e683fffcd80b247c148796404e019e404d4
Kasra Kassai <seekasra@gmail.com>
2019-01-14 22:18:43 +0000

    play with range()w

--- python/standard_library/operators.py
+++ python/standard_library/operators.py
@@ -1,25 +1,13 @@
-# Python Logical Operators: And, Or, Not:
 
-# What is a Boolean?
-isRaining = True
-isSunny = False
-# Logical Operators -> Special Operators for Booleans
-# AND
-# true and true --> true
-# false and true --> false
-# true and false --> false
-# false and false --> false
-if isRaining and isSunny:
-    print("We may see a rainbow!")
-# OR
-# true and true --> true
-# false and true --> true
-# true and false --> true
-# false and false --> false
-if isRaining or isSunny:
-    print("We may see a rainbow!")
-# NOT
-# true --> false
-# false --> true
-if not isSunny:
-    print("it is not sunny")
+
+
+
+ages = list(range(50))
+for age in ages: 
+    isAdult = age > 17
+    print(f"{age} is not an adult!") if not isAdult else ""
+
+register = {"kasra": 25, "brad": 24, "alireza": 17}
+r = range(30)
+print("1111111")
+print(ages)
