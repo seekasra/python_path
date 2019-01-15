3950f1979974a10c4a7b8265930cd3141fe6b08d
Kasra Kassai <seekasra@gmail.com>
2019-01-15 22:12:22 +0000

    add random

--- python/math_test.py
+++ python/math_test.py
@@ -1,3 +1,13 @@
 import math
+import random
 
-print(math.pi)
+
+print(math.inf)
+print(math.factorial(10))
+
+print(math.gcd(1231,36))
+
+print(random.random())
+
+print("D I C E")
+print("You rolled :" + str(random.randrange(1,7)))
